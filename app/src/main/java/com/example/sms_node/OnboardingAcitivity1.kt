7c27640b2b3c3952e7fdb9209e602aa0d39d4b29
Cjay Salgado <cjaysalgado07@gmail.com>
2026-07-07 01:33:19 +0800

    feat: App splash screen & App icon added

--- app/src/main/java/com/example/sms_node/OnboardingAcitivity1.kt
+++ app/src/main/java/com/example/sms_node/OnboardingAcitivity1.kt
@@ -1,37 +1,20 @@
 package com.example.sms_node
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class OnboardingAcitivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
-
-        val prefs = getSharedPreferences("app", MODE_PRIVATE)
-        val firstLaunch = !prefs.getBoolean("onboarding_completed", false);
-
-        if(firstLaunch){
-            val intent = Intent(this, OnboardingAcitivity1::class.java)
-            startActivity(intent)
-            finish()
-            super.onCreate(savedInstanceState)
-            return
-        }
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding_acitivity1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
     }
 }
